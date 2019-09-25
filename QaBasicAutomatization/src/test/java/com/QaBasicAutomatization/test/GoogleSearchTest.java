af580320c080a9b884360f8f6e88ba3c98149ad9
Felipe Burbano <felipeburbano16@gmail.com>
2019-09-24 23:21:16 -0500

    Ejemplo basico de automatizacion web

---
+++ QaBasicAutomatization/src/test/java/com/QaBasicAutomatization/test/GoogleSearchTest.java
@@ -0,0 +1,50 @@
+package com.QaBasicAutomatization.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+	
+	//Lo primero es crear un objeto de tipo WebDriver
+	private WebDriver driver;
+	//Se crea el evento y parametrizaciones antes de ejecutar la prueba
+	@Before
+	public void setUp() {		
+		//setiar el ejecutable
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");		
+		//declara el objeto de tipo ChromeDriver
+		driver = new ChromeDriver();		
+		//maximizar la ventana del navegador
+		driver.manage().window().maximize();		
+		//abrir la web a la cual vamos a realizar la prueba
+		driver.get("https://www.google.com/");
+	}	
+	//Se crean los eventos donde realizaremos nuestros casos de prueba
+	@Test
+	public void testGooglePage() {		
+		//Se crean los web elements para la ejecución
+		WebElement searchbox = driver.findElement(By.name("q"));
+		searchbox.clear();
+		searchbox.sendKeys("Selenium");
+		searchbox.submit();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		assertEquals("Selenium - Buscar con Google", driver.getTitle());		
+	}	
+	//Se crean los enventos para despues de la ejecución
+	@After
+	public void tearDown() {
+		//cierra el navegador una vez completado el caso de prueba
+		driver.quit();
+	}
+	
+	
+}
